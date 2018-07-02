cb01b5f32756e8efc1508fd8da6381899e9b86ae
virco <dev.virco@gmail.com>
2018-07-01 22:16:18 -0400

    Creating the central access point for the data layer

---
+++ data/src/main/java/com/marcobrenes/data/ProjectsDataRepository.kt
@@ -0,0 +1,54 @@
+package com.marcobrenes.data
+
+import com.marcobrenes.data.mapper.ProjectMapper
+import com.marcobrenes.data.repository.ProjectsCache
+import com.marcobrenes.data.store.ProjectsDataStoreFactory
+import com.marcobrenes.domain.model.Project
+import com.marcobrenes.domain.repository.ProjectsRepository
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import javax.inject.Inject
+
+class ProjectsDataRepository @Inject constructor(
+        private val mapper: ProjectMapper,
+        private val cache: ProjectsCache,
+        private val factory: ProjectsDataStoreFactory
+) : ProjectsRepository {
+
+    override fun getProjects(): Observable<List<Project>> {
+        return Observable.zip(cache.areProjectsCached().toObservable(),
+                cache.isProjectCacheExpired().toObservable(),
+                BiFunction<Boolean, Boolean, Pair<Boolean, Boolean>> { areCached, isExpired ->
+                    Pair(areCached, isExpired)
+                })
+                .flatMap {
+                    factory.getDataStore(it.first, it.second).getProjects()
+                }
+                .flatMap { projects ->
+                    factory.getCachedDataStore()
+                            .saveProjects(projects)
+                            .andThen(Observable.just(projects))
+                }
+                .map {
+                    it.map {
+                        mapper.mapFromEntity(it)
+                    }
+                }
+    }
+
+    override fun bookmarkProject(projectId: String): Completable {
+        return factory.getCachedDataStore().setProjectAsBookmarked(projectId)
+    }
+
+    override fun unbookmarkProject(projectId: String): Completable {
+        return factory.getCachedDataStore().setProjectAsNotBookmarked(projectId)
+    }
+
+    override fun getBookmarkedProjects(): Observable<List<Project>> {
+        return factory.getCachedDataStore().getBookmarkedProjects()
+                .map {
+                    it.map { mapper.mapFromEntity(it) }
+                }
+    }
+}
