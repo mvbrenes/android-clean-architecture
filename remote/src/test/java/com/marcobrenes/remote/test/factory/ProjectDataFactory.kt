0b4d788b7f2b325f027808f182d438c6e224e5e7
marco <marcovbrenes@gmail.com>
2018-07-04 22:44:29 -0400

    Setting up the Remote layer for testing

---
+++ remote/src/test/java/com/marcobrenes/remote/test/factory/ProjectDataFactory.kt
@@ -0,0 +1,33 @@
+package com.marcobrenes.remote.test.factory
+
+import com.marcobrenes.data.model.ProjectEntity
+import com.marcobrenes.remote.model.OwnerModel
+import com.marcobrenes.remote.model.ProjectModel
+import com.marcobrenes.remote.model.ProjectsReponseModel
+
+object ProjectDataFactory {
+
+    fun makeOwner(): OwnerModel {
+        return with(DataFactory) {
+            OwnerModel(randomString(), randomString())
+        }
+    }
+
+    fun makeProject(): ProjectModel {
+        return with(DataFactory) {
+            ProjectModel(randomString(), randomString(), randomString(),
+                    randomInt(), randomString(), makeOwner())
+        }
+    }
+
+    fun makeProjectEntity(): ProjectEntity {
+        return with(DataFactory) {
+            ProjectEntity(randomString(), randomString(), randomString(),
+                    randomString(), randomString(), randomString(), randomString())
+        }
+    }
+
+    fun makeProjectsResponse(): ProjectsReponseModel {
+        return ProjectsReponseModel(listOf(makeProject(), makeProject()))
+    }
+}
