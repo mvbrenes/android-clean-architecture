73c318d1e2ce6f547673b32df123e43540b1496f
marco <marcovbrenes@gmail.com>
2018-07-11 20:31:45 -0400

    Creating the Projects Cache data source Implementation

---
+++ cache/src/main/java/com/marcobrenes/cache/ProjectsCacheImpl.kt
@@ -0,0 +1,77 @@
+package com.marcobrenes.cache
+
+import com.marcobrenes.cache.db.ProjectsDatabase
+import com.marcobrenes.cache.mapper.CachedProjectMapper
+import com.marcobrenes.cache.model.Config
+import com.marcobrenes.data.model.ProjectEntity
+import com.marcobrenes.data.repository.ProjectsCache
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import javax.inject.Inject
+
+
+class ProjectsCacheImpl @Inject constructor(
+        private val projectsDatabase: ProjectsDatabase,
+        private val mapper: CachedProjectMapper) : ProjectsCache {
+
+    override fun clearProjects(): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().deleteProjects()
+            Completable.complete()
+        }
+    }
+
+    override fun saveProjects(projects: List<ProjectEntity>): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().insertProjects(
+                    projects.map { mapper.mapToCached(it) })
+            Completable.complete()
+        }
+    }
+
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        return projectsDatabase.cachedProjectsDao().getProjects()
+                .toObservable()
+                .map { it.map { mapper.mapFromCached(it) } }
+    }
+
+    override fun getBookmarkedProjects(): Observable<List<ProjectEntity>> {
+        return projectsDatabase.cachedProjectsDao().getBookmarkedProjects()
+                .toObservable()
+                .map { it.map { mapper.mapFromCached(it) } }
+    }
+
+    override fun setProjectAsBookmarked(projectId: String): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().setBookmarkStatus(true, projectId)
+            Completable.complete()
+        }
+    }
+
+    override fun setProjectAsNotBookmarked(projectId: String): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().setBookmarkStatus(false, projectId)
+            Completable.complete()
+        }
+    }
+
+    override fun areProjectsCached(): Single<Boolean> {
+        return projectsDatabase.cachedProjectsDao().getProjects().isEmpty.map { !it }
+    }
+
+    override fun setLastCacheTime(lastCache: Long): Completable {
+        return Completable.defer {
+            projectsDatabase.configDao().insertConfig(Config(lastCacheTime = lastCache))
+            Completable.complete()
+        }
+    }
+
+    override fun isProjectsCacheExpired(): Single<Boolean> {
+        val currentTime = System.currentTimeMillis()
+        val expirationTime = (60 * 10 * 1000).toLong()
+        return projectsDatabase.configDao().getConfig()
+                .single(Config(lastCacheTime = 0))
+                .map { currentTime - it.lastCacheTime > expirationTime }
+    }
+}
--- cache/src/main/java/com/marcobrenes/cache/dao/ConfigDao.kt
+++ cache/src/main/java/com/marcobrenes/cache/dao/ConfigDao.kt
@@ -6,12 +6,13 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.marcobrenes.cache.db.ConfigConstants.QUERY_CONFIG
 import com.marcobrenes.cache.model.Config
+import io.reactivex.Flowable
 
 @Dao
 abstract class ConfigDao {
 
     @Query(QUERY_CONFIG)
-    abstract fun getConfig(): Config
+    abstract fun getConfig(): Flowable<Config>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertConfig(config: Config)
