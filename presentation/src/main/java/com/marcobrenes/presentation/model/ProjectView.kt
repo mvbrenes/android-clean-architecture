2063eca6bde2c1b3f43f32c9256ba25a27c1c39a
marco <marcovbrenes@gmail.com>
2018-07-13 20:54:01 -0400

    Creating the data models for the presentation layer

---
+++ presentation/src/main/java/com/marcobrenes/presentation/model/ProjectView.kt
@@ -0,0 +1,10 @@
+package com.marcobrenes.presentation.model
+
+class ProjectView(val id: String,
+                  val name: String,
+                  val fullName: String,
+                  val starCount: String,
+                  val dateCreated: String,
+                  val ownerName: String,
+                  val ownerAvatar: String,
+                  val isBookmarked: Boolean)
