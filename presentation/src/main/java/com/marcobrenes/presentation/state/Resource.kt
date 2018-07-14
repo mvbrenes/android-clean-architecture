404df871f63c82acbbd945004b838e03f6c56b74
marco <marcovbrenes@gmail.com>
2018-07-13 21:21:07 -0400

    Creating the browse projects view model

--- presentation/src/main/java/com/marcobrenes/presentation/state/Resource.kt
+++ presentation/src/main/java/com/marcobrenes/presentation/state/Resource.kt
@@ -2,5 +2,5 @@ package com.marcobrenes.presentation.state
 
 class Resource<out T> constructor(
         val status: ResourceState,
-        val data: T,
-        val message: String?)
+        val data: T? = null,
+        val message: String? = null)
