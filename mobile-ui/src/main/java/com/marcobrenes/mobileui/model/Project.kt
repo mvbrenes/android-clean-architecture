afcc06b1d2cdeed0bcaa884f4c325170f8544059
marco <marcovbrenes@gmail.com>
2018-07-15 00:02:26 -0400

    Creating the UI modules Project representation

---
+++ mobile-ui/src/main/java/com/marcobrenes/mobileui/model/Project.kt
@@ -0,0 +1,10 @@
+package com.marcobrenes.mobileui.model
+
+class Project(val id: String,
+              val name: String,
+              val fullName: String,
+              val starCount: String,
+              val dateCreated: String,
+              val ownerName: String,
+              val ownerAvatar: String,
+              val isBookmarked: Boolean)
